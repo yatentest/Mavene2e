b8470eb835807b16b440186b2942b4b5edba65cc
Yaten <yaten.usa@gmail.com>
2017-12-30 20:47:51 -0500

    Commite2e

---
+++ MavenTest/src/test/java/automationDemo/Test1.java
@@ -0,0 +1,12 @@
+package automationDemo;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void browsertest()
+	{
+		System.out.println("first test");
+	}
+}
---
+++ MavenTest/src/test/java/automationDemo/Test2.java
@@ -0,0 +1,11 @@
+package automationDemo;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void browsertest2()
+	{
+		System.out.println("sedcond test");
+	}
+}
